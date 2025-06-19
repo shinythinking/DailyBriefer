3e826b1c659c1fba0b31572a0874c983d621763e
shinythinking <shinythink@gmail.com>
2025-06-20 01:36:14 +0900

    feat: Construct TaskCard UI
    TaskCard Shows whether today's tasks have been completed.
    Status is enum class which has two type. One is for Done!. The other is Not Yet.

---
+++ src/main/java/shinythinking/view/Status.java
@@ -0,0 +1,24 @@
+package shinythinking.view;
+
+import java.awt.*;
+
+enum Status {
+    DONE("Done!!", Color.GREEN),
+    NOT_YET("NOT YET!", Color.RED);
+
+    private final String msg;
+    private final Color color;
+
+    Status(String msg, Color color) {
+        this.msg = msg;
+        this.color = color;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/shinythinking/view/TaskCard.java
@@ -0,0 +1,43 @@
+package shinythinking.view;
+
+import shinythinking.model.Task;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TaskCard extends JPanel {
+    public TaskCard(Task task) {
+        setLayout(new GridBagLayout());
+        setBackground(Color.WHITE);
+        setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 4));
+
+        Status status;
+        if(task.isDone()) {
+            status = Status.DONE;
+        } else {
+            status = Status.NOT_YET;
+        }
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1.0;
+
+        JLabel nameLabel = new JLabel(task.getTitle(), SwingConstants.CENTER);
+        JLabel statusLabel = new JLabel(status.getMsg(), SwingConstants.CENTER);
+        JSeparator separator = new JSeparator();
+
+        gbc.gridy = 0;
+        gbc.weighty = 1.0;
+        add(nameLabel, gbc);
+
+        gbc.gridy = 1;
+        gbc.weighty = 0.0;
+        add(separator, gbc);
+
+        gbc.gridy = 2;
+        gbc.weighty = 3.0;
+        statusLabel.setForeground(status.getColor());
+        add(statusLabel, gbc);
+    }
+}

24d63af33dd6bd29946630c02d2ae843ee9d694e
shinythinking <shinythink@gmail.com>
2025-06-20 01:17:55 +0900

    feat: Construct FileIO class
    This class is for FileIO. It saves taskModel. It also loads taskModel from Local to initialize the taskModel.

---
+++ src/main/java/shinythinking/io/LocalIO.java
@@ -0,0 +1,40 @@
+package shinythinking.io;
+
+import shinythinking.model.Task;
+import shinythinking.model.TaskModel;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class LocalIO {
+    GsonConverter gson = new GsonConverter();
+    Path dirPath = Paths.get("taskdata");
+    Path filePath = dirPath.resolve("taskmodel.json");
+
+    public void saveTaskModel(TaskModel taskModel) {
+        try {
+            Files.createDirectories(dirPath);
+
+            String json = gson.serialize(taskModel);
+            Files.writeString(filePath, json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public TaskModel loadTaskModel() {
+        try {
+            if (!Files.exists(filePath)) {
+                return null;
+            }
+
+            String json = Files.readString(filePath);
+            return gson.deserialize(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}

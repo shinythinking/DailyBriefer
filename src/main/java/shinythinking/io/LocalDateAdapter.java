bf802fdd9d0cc57a09ae3a41c6e5e0e7b93edfcd
shinythinking <shinythink@gmail.com>
2025-06-20 01:16:16 +0900

    feat: Adapter class for Serialization
    This class enable the LocalDate serializable.

---
+++ src/main/java/shinythinking/io/LocalDateAdapter.java
@@ -0,0 +1,22 @@
+package shinythinking.io;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+
+    @Override
+    public JsonElement serialize(LocalDate date, Type type, JsonSerializationContext context) {
+        return new JsonPrimitive(date.format(formatter));
+    }
+
+    @Override
+    public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+        return LocalDate.parse(json.getAsString(), formatter);
+    }
+}
